8fa8d0897476980c666b8acd225a4a70593387bb
박우빈 <62981093+yunaluv358@users.noreply.github.com>
2020-07-20 20:46:30 +0900

    20:46

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 import {Member} from './pages'
 import Counter from "./pages/Counter";
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import Greeting from './components/Greeting'
+import EventPractice from "./components/EventPractice";
+import SigninTest from "./components/SigninTest";
+import Signin from "./components/Signin";
+import HideTest from "./components/HideTest";
 const App = () => <div className="App">
-  <Member/>
+  <HideTest/>
 </div>
-
-
 
 export default App;
---
+++ src/components/SigninTest.tsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from "react";
+
+const SignTest:React.FC = () =>{
+    const [userid, setUserid] = useState('')
+    const [password, setPassword] = useState('')
+
+    // useEffect 컴포넌트가 랜더링 될 때마다 특정 작업을 수행하도록 설정
+    useEffect(()=>{
+        alert(`[] 로 하면 처음 실행시 한번만 작동함`)
+    }, [])
+    const onChangeUserid = e => {
+        setUserid(e.target.value)
+    }
+    const onChangePassword = e => {
+        setPassword(e.target.value)
+    }
+
+    return (
+        <>
+            <div>
+                <input type="text" value={userid} onChange={onChangeUserid}/>
+                <input type="text" value={password} onChange={onChangePassword}/>
+            </div>
+            <div>
+                <b>아이디: </b> {userid} <br/>
+                <b>비밀번호: </b> {password} <br/>
+
+            </div>
+        </>
+    )
+}
+export default SignTest
